1927122e50cebf22a3424ec40e7946e2a503a644
jiannlee22 <947807614@qq.com>
2023-11-29 13:45:11 +0800

    autostart
    (cherry picked from commit 1d1402b8176fa80a3a57b4cfd0c85edd221c7936)

--- config.def.h
+++ config.def.h
@@ -14,6 +14,12 @@ static const float urgentcolor[]           = COLOR(0xff0000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You can also use glsl colors */
 
+/* Autostart */
+static const char *const autostart[] = {
+        "wbg", "/path/to/your/image", NULL,
+        NULL /* terminate */
+};
+
 /* tagging - TAGCOUNT must be no greater than 31 */
 #define TAGCOUNT (9)
 
